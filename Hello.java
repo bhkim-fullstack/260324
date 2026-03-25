fce9b83f707fdf375b60c543c1abcdb7ed3fff48
BH <kbh02180@gamil.com>
2026-03-25 18:14:22 +0900

    자바 파일 추가

---
+++ Hello.java
@@ -0,0 +1,16 @@
+package kr.com.beak;
+
+public class Hello {
+
+	// method 
+	public void print(String name) {
+		System.out.println("Hello, ");
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
